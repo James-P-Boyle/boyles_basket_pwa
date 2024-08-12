f60f96a309578849ddf3d9dba89ab6084c63cd6a
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2024-08-12 11:44:36 +0200

    Refactor use list hook

--- src/hooks/useList.tsx
+++ src/hooks/useList.tsx
@@ -7,31 +7,32 @@ export default function useList(id?: string) {
 
   const addNewList = useCallback((newList: List) => {
     setLists(prev => prev ? [newList, ...prev] : [ newList ])
-  }, [])
+  }, [setLists])
+
+  // const updateLists = useCallback((updatedList: List) => {
+  //   setLists(prevLists => prevLists.map(l => l.id === id ? updatedList : l))
+  // }, [setLists])
+
+  const updateItems = useCallback((updateFn: (items: Item[]) => Item[]) => {
+    setLists(prevLists => prevLists.map(list => {
+      if (list.id !== id) return list
+      const updatedList = {
+        ...list,
+        items: updateFn(list.items),
+      }
+      return updatedList
+    }))
+  }, [id, setLists])
+
+  const addNewItem = useCallback((newItem: Item) => {
+    updateItems(items => [...items, newItem])
+  }, [updateItems])
+
+  const deleteItem = useCallback((itemToDelete: Item) => {
+    updateItems(items => items.filter(item => item !== itemToDelete))
+  }, [updateItems])
 
   const list = useMemo(() => lists.find(list => list.id === id), [lists, id])
 
-  const updateLists = useCallback((updatedList: List) => {
-    setLists(prevLists => prevLists.map(l => l.id === id ? updatedList : l))
-  }, [id])
-
-  const addNewItem = useCallback((newItem: Item) => {
-    if (!list) return
-    const updatedList: List = {
-      ...list,
-      items: [...list.items, newItem],
-    }
-    updateLists(updatedList)
-  }, [list, updateLists])
-
-  const deleteItem = useCallback((item: Item) => {
-    if (!list) return
-    const updatedList: List = {
-      ...list,
-      items: list.items.filter(i => i !== item),
-    }
-    updateLists(updatedList)
-  }, [list, updateLists])
-
-  return {lists, addNewList, list, addNewItem, deleteItem}
+  return {lists, list, addNewList, addNewItem, deleteItem}
 }
