fbffd24fab7004dca28f6296c0005ae3863a0f5a
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2024-08-13 09:05:39 +0200

    Remove comment

--- src/components/lists/Show.tsx
+++ src/components/lists/Show.tsx
@@ -6,40 +6,21 @@ import { useContext, useEffect } from "react"
 import { HeaderContext } from "../../contexts/HeaderContext"
 import DeleteButton from "../DeleteButton"
 import EditButton from "../EditButton"
-import { Item } from "../../App"
+import { List } from "../../App"
 
 export default function Show() {
   const { id } = useParams<{ id: string }>()
   const { list, addNewItem, deleteItem, deleteList, updateListName, updateItem } = useList(id!)
   const headerContext = useContext(HeaderContext);
 
-  // const handleListNameChange = (newName: string) => {
-  //   updateListName(newName)
-  // }
-
-  // const handleUpdateItem = (itemId: string, updatedFields: Partial<Item>) => {
-  //   updateItem(itemId, updatedFields)
-  // }
-
   useEffect(() => {
     if(!list) return
     headerContext?.setHeaderContent(
-      <>
-        <span className="date">{new Date(list!.createdAt).toLocaleDateString()}</span>
-        <div className="listTitle">
-        <EditButton
-          onSubmit={updateListName}
-          label={`Rename "${list.name}"`}
-        />
-        <h1>{list!.name}</h1>
-        <DeleteButton
-          onCLick={() => deleteList(list!.id!)}
-          confirmBeforeDelete
-          deleteMessage="Are you sure you want to delete this list?"
-        />
-
-        </div>
-      </>
+      <ShowHeader
+        list={list}
+        updateListName={updateListName}
+        deleteList={deleteList}
+      />
     )
     return () => headerContext?.setHeaderContent(null)
   }, [list, deleteList])
@@ -72,5 +53,33 @@ export default function Show() {
         )}
       </div>
     </div>
+  )
+}
+
+const ShowHeader = ({
+  list,
+  updateListName,
+  deleteList
+} : {
+  list: List,
+  updateListName: (newName: string) => void,
+  deleteList: (listId: string) => void
+}) => {
+  return (
+    <>
+      <span className="date">{new Date(list!.createdAt).toLocaleDateString()}</span>
+      <div className="listTitle">
+        <EditButton
+          onSubmit={updateListName}
+          label={`Rename "${list.name}"`}
+        />
+        <h1>{list!.name}</h1>
+        <DeleteButton
+          onCLick={() => deleteList(list!.id!)}
+          confirmBeforeDelete
+          deleteMessage="Are you sure you want to delete this list?"
+        />
+      </div>
+    </>
   )
 }
