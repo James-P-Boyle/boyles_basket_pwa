cf45acb849092c26f18d0b14c4e59cf4ba809495
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2024-08-14 08:43:54 +0200

    Restyle create button

--- src/components/lists/Create.tsx
+++ src/components/lists/Create.tsx
@@ -33,8 +33,13 @@ export default function Create({
 
   return (
     <div id="createList">
-      <button onClick={handleCreateList}>
-        Create New List
+      <button
+        className="roundedButton"
+        onClick={handleCreateList}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2}>
+          <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
       </button>
     </div>
   )
