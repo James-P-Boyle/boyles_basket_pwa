ca5361325cc60cb2ec19b34ca48bdf407a186d6e
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2024-08-15 09:24:57 +0200

    Deploy to GitHub Pages

---
+++ dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://james-p-boyle.github.io/shopping_planner_pwa/sw.js', { scope: 'https://james-p-boyle.github.io/shopping_planner_pwa/' })})}
