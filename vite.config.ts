cdedb02254fed522a2cb4136a4d838bd275f165b
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2024-08-19 11:01:35 +0200

    Refactor vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
 import path from 'path';
 
+const base = '/shopping_planner_pwa/'
+
 const manifestForPlugin: Partial<VitePWAOptions> = {
   registerType:'prompt',
   includeAssets:[
@@ -17,48 +19,48 @@ const manifestForPlugin: Partial<VitePWAOptions> = {
     "maskable_icon_512x512.png"
   ],
   manifest: {
-    name:"TheBoylesPWA",
-    short_name:"The-Boyles-Pwa",
+    name:"Boyle's Basket",
+    short_name:"Boyle's Basket",
     description:"I am a shopping app, made by the boyle",
     icons: [
       {
-        src: '/shopping_planner_pwa/android-chrome-192x192.png',
+        src: `${base}android-chrome-192x192.png`,
         sizes: '192x192',
         type: 'image/png',
         purpose: 'any'
       },
       {
-        src: '/shopping_planner_pwa/android-chrome-512x512.png',
+        src: `${base}android-chrome-512x512.png`,
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any'
       },
       {
-        src: '/shopping_planner_pwa/apple-touch-icon.png',
+        src:  `${base}apple-touch-icon.png`,
         sizes: '180x180',
         type: 'image/png',
         purpose: 'any'
       },
       {
-        src: '/shopping_planner_pwa/favicon-16x16.png',
+        src: `${base}favicon-16x16.png`,
         sizes: '16x16',
         type: 'image/png',
         purpose: 'any'
       },
       {
-        src: '/shopping_planner_pwa/favicon-32x32.png',
+        src: `${base}favicon-32x32.png`,
         sizes: '32x32',
         type: 'image/png',
         purpose: 'any'
       },
       {
-        src: '/shopping_planner_pwa/maskable_icon_192x192.png',
+        src: `${base}maskable_icon_192x192.png`,
         sizes: '192x192',
         type: 'image/png',
         purpose: 'maskable'
       },
       {
-        src: '/shopping_planner_pwa/maskable_icon_512x512.png',
+        src: `${base}maskable_icon_512x512.png`,
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable'
