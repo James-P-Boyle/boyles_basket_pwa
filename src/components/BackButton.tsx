b8c1ec20ee9ea8016ac57a5a21ca9f081f07cecd
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2024-08-22 12:07:34 +0200

    Fix nav links

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -1,16 +1,11 @@
-import { useLocation, useNavigate } from "react-router-dom"
+import { useLocation } from "react-router-dom"
 
 export default function BackButton() {
-
-  const navigate = useNavigate()
-  const location = useLocation();
+  const location = useLocation()
   const showBackButton = location.pathname !== "/"
-  const handleBack = () => {
-    navigate(-1)
-  }
 
   return showBackButton && (
-    <button id="backButton" onClick={handleBack}>
+    <button className="backButton">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" strokeWidth={3} fill="none">
         <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
       </svg>
